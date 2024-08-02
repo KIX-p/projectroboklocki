b2e85bdf9a2e5bdfc2740e0db45fd686278906e2
Pawel Kikh <pavlokiht2005@gmail.com>
2024-08-02 21:03:15 +0200

    added footer

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("form1/", views.form1, name="form1"),
+    path("form2/", views.form2, name="form2"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def form1(request):
+    return render(request, 'main/form1.html')
+
+def form2(request):
+    return render(request, 'main/form2.html')
