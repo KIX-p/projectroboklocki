c9f4d347b548b9edb8fe68e0bc127ffaf145062d
Pawel Kikh <pavlokiht2005@gmail.com>
2024-08-07 22:09:14 +0200

    updated designe and responsisive

--- legoproject/settings.py
+++ legoproject/settings.py
@@ -125,12 +125,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False 
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pavlokiht2005@gmail.com'
-EMAIL_HOST_PASSWORD = 'rvmv xtvb vfpv qljq'
--- main/views.py
+++ main/views.py
@@ -20,33 +20,3 @@ def form1(request):
 def form2(request):
     return render(request, 'main/form2.html')
 
-def send_email(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            data = {
-                'name': form.cleaned_data['name'],
-                'email': form.cleaned_data['email'],
-                'location': form.cleaned_data['location'],
-                'message': form.cleaned_data['message'],
-            }
-
-            #Send email
-            html_body = render_to_string('email_templates/appointment.html', data)
-
-            msg = EmailMultiAlternatives(
-                subject='Appointment',
-                body=html_body,
-                from_email = data['email'],
-                to=['pavlokiht2005@gmail.com'],
-                reply_to=[data['email']])
-                
-            msg.attach_alternative(html_body, "text/html")
-            msg.send()
-
-            #return JsonResponse(data)
-            return JsonResponse({'success': data})
-        else:
-            return JsonResponse({'error': form.errors})
-    return redirect('index')
-
